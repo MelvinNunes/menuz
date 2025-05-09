34f2b57905c8e97c90f7a5ea62b22312956f8a8d
Melvin Nunes <melvinnunes14@gmail.com>
2025-05-09 21:28:53 +0200

    feat: added more fields to Configuration table

--- Domain/Entities/Configuration.cs
+++ Domain/Entities/Configuration.cs
@@ -3,4 +3,7 @@ namespace Domain.Entities;
 public class Configuration
 {
     public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public required string Value { get; set; }
 }
